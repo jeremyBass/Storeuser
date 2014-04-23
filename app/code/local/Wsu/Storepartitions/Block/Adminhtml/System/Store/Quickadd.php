52a0d35a43f1304001b35091b0eaef9ab6c81f23
jeremyBass <jeremybass@cableone.net>
2014-04-23 13:44:37 -0700

    preps the form for quickadd

--- app/code/local/Wsu/Storepartitions/Block/Adminhtml/System/Store/Quickadd.php
+++ app/code/local/Wsu/Storepartitions/Block/Adminhtml/System/Store/Quickadd.php
@@ -1,6 +1,6 @@
 <?php
 
-class Wsu_Storepartitions_Block_Adminhtml_System_Store_Quickadd extends Mage_Adminhtml_Block_System_Store_Edit {
+class Wsu_Storepartitions_Block_Adminhtml_System_Store_Quickadd extends Mage_Adminhtml_Block_Widget_Form_Container {
     /**
      * Init class
      *
@@ -8,25 +8,18 @@ class Wsu_Storepartitions_Block_Adminhtml_System_Store_Quickadd extends Mage_Adm
     public function __construct() {
 		$this->_objectId = 'website_id';
 		$saveLabel   = Mage::helper('core')->__('Save Website');
-		$deleteLabel = Mage::helper('core')->__('Delete Website');
-		$deleteUrl   = $this->getUrl('*/*/deleteWebsite', array('item_id' => Mage::registry('store_data')->getId()));
         $this->_controller = 'system_store';
-
         parent::__construct();
-
         $this->_updateButton('save', 'label', $saveLabel);
-        $this->_updateButton('delete', 'label', $deleteLabel);
-        $this->_updateButton('delete', 'onclick', 'setLocation(\''.$deleteUrl.'\');');
 
-        if (!Mage::registry('store_data')->isCanDelete()) {
-            $this->_removeButton('delete');
-        }
-        if (Mage::registry('store_data')->isReadOnly()) {
-            $this->_removeButton('save')->_removeButton('reset');
-        }
     }
 
-    /**
+    protected function _prepareLayout() {
+        /* Add website button */
+        $this->setChild('form', $this->getLayout()->createBlock('adminhtml/system_store_quickadd_form'));
+        return parent::_prepareLayout();
+    }
+   /**
      * Get Header text
      *
      * @return string
