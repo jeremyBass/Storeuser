db373a83448a5e9d5dff0bbdb52a5fa0e2cf381d
jeremyBass <jeremybass@cableone.net>
2014-04-23 15:44:47 -0700

    set up the form from with in the construct

--- app/code/local/Wsu/Storepartitions/Block/Adminhtml/System/Store/Quickadd.php
+++ app/code/local/Wsu/Storepartitions/Block/Adminhtml/System/Store/Quickadd.php
@@ -8,17 +8,17 @@ class Wsu_Storepartitions_Block_Adminhtml_System_Store_Quickadd extends Mage_Adm
     public function __construct() {
 		$this->_objectId = 'website_id';
 		$saveLabel   = Mage::helper('core')->__('Save Website');
-        $this->_controller = 'system_store';
+
+		$this->_blockGroup = 'adminhtml';
+		$this->_controller = 'system_store';
+		$this->_mode = 'quickadd';
+		
         parent::__construct();
+		 
         $this->_updateButton('save', 'label', $saveLabel);
 
     }
 
-    protected function _prepareLayout() {
-        /* Add website button */
-        $this->setChild('form', $this->getLayout()->createBlock('adminhtml/system_store_quickadd_form'));
-        return parent::_prepareLayout();
-    }
    /**
      * Get Header text
      *
