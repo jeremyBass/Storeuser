4b27fd0c0d2998f6316ad5d56bcb889919bbb3be
jeremyBass <jeremybass@cableone.net>
2014-04-23 16:54:37 -0700

    clears out some of the alerts for t

--- skin/adminhtml/base/default/wsu/storepartitions/js/admin_helper.js
+++ skin/adminhtml/base/default/wsu/storepartitions/js/admin_helper.js
@@ -2,19 +2,22 @@
 jQuery(function(){
 	(function($,window){
 		var formBtn = $('button.scalable.quickadd');
-		var url = formBtn.attr("onclick").split("'")[1];
 		
+		var onclick = formBtn.attr("onclick");
+		var url = onclick.split("'")[1];
 		formBtn.removeAttr('onclick');
 		
-		formBtn.on('click',function(e){
+		formBtn.off().on('click',function(e){
+			alert('clicky');
 			e.preventDefault();
+			e.stopPropagation();
 			//	setLocation('http://store.mage.dev/index.php/admin/system_store/quickAdd/key/281094b000c95628f9dadc04c834e1a8/')
-			if($( "#quickAddForm" ).lengh<=0){
+			if($( "#quickAddForm" ).length<=0){
 				$('body').append('<div id="HOLDING" style="display:none;" />');
 				$('#HOLDING').append('<div id="quickAddForm" />');
 			}
 			
-			$('#quickAddForm').load(url+" #anchor-content",{},function(res){
+			$.post(url+" #anchor-content",{},function(res){
 				$( "#quickAddForm" ).dialog({
 					height: $(window).height()*.80,
 					width: $(window).width()*.80,
