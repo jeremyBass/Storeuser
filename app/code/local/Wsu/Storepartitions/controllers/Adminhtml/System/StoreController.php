edeacfb52bec75a451f026d7cf901d00c5588f83
jeremyBass <jeremybass@cableone.net>
2014-04-24 08:25:58 -0700

    adds an error message for the root cat

--- app/code/local/Wsu/Storepartitions/controllers/Adminhtml/System/StoreController.php
+++ app/code/local/Wsu/Storepartitions/controllers/Adminhtml/System/StoreController.php
@@ -91,7 +91,7 @@ class Wsu_Storepartitions_Adminhtml_System_StoreController extends Mage_Adminhtm
 					}else{Mage::getSingleton('adminhtml/session')->addError("failed to create the store view");}
 				}else{Mage::getSingleton('adminhtml/session')->addError("failed to create the store group");}
 			}else{Mage::getSingleton('adminhtml/session')->addError("failed to create the website");}
-		}
+		}else{Mage::getSingleton('adminhtml/session')->addError("failed to create the root category");}
 
 		$this->_redirect('*/*/');
 		
