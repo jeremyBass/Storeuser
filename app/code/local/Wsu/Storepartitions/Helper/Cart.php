9cd1ca74d2f946520a356f73f49f24c9d5891773
jeremyBass <jeremybass@cableone.net>
2014-04-29 15:20:08 -0700

    puts in the cart rewriter

---
+++ app/code/local/Wsu/Storepartitions/Helper/Cart.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Checkout url helper
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Wsu_Storepartitions_Helper_Cart extends Mage_Checkout_Helper_Cart {
+    /**
+     * Retrieve shopping cart url
+     *
+     * @return string
+     */
+    public function getCartUrl() {
+        $storeId = 1; //pick up from setting.. look to latter but hard code now
+        return Mage::app()->getStore($storeId)->getUrl('checkout/cart');
+        return $this->_getUrl('checkout/cart');
+    }
+}
