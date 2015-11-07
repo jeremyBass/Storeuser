51793ae75f88bbd7d3f1f1f6357fa54bcdc1e6d4
Jeremy Bass <jeremybass@cableone.net>
2015-11-06 22:33:16 -0800

    update the validator that will require cats on product save

--- app/code/local/Wsu/Storepartitions/controllers/Adminhtml/CatalogProductController.php
+++ app/code/local/Wsu/Storepartitions/controllers/Adminhtml/CatalogProductController.php
@@ -1,6 +1,5 @@
 <?php
 class Wsu_Storepartitions_Adminhtml_CatalogProductController extends Mage_Adminhtml_Controller_Action {
-
     public function massOwnerAction() {
         $productIds = (array)$this->getRequest()->getParam('product');
         $storeId    = (int)$this->getRequest()->getParam('store', 0);
@@ -27,5 +26,75 @@ class Wsu_Storepartitions_Adminhtml_CatalogProductController extends Mage_Adminh
             $this->_getSession()->addException($e, $this->__('An error occurred while updating the product(s) owners.'));
         }
         $this->_redirect('adminhtml/catalog_product/', array('store'=> $storeId));
-    }     
+    }
+	public function validateAction() {
+		$response = new Varien_Object();
+		$response->setError(false);
+	
+		try {
+			$productData = $this->getRequest()->getPost('product');
+	
+			if ($productData && !isset($productData['stock_data']['use_config_manage_stock'])) {
+				$productData['stock_data']['use_config_manage_stock'] = 0;
+			}
+			/* @var $product Mage_Catalog_Model_Product */
+			$product = Mage::getModel('catalog/product');
+			$product->setData('_edit_mode', true);
+			if ($storeId = $this->getRequest()->getParam('store')) {
+				$product->setStoreId($storeId);
+			}
+			if ($setId = $this->getRequest()->getParam('set')) {
+				$product->setAttributeSetId($setId);
+			}
+			if ($typeId = $this->getRequest()->getParam('type')) {
+				$product->setTypeId($typeId);
+			}
+			if ($productId = $this->getRequest()->getParam('id')) {
+				$product->load($productId);
+			}
+			//==>start my changes
+			$postedCategories = $this->getRequest()->getParam('category_ids');
+			if ($product->getId()) { //if edit mode 
+				//if product does not have categories and no categories were sent...
+				if (!$product->getCategoryIds() && empty($postedCategories)){
+					throw new Mage_Catalog_Exception('Fill in categories'); //translate if needed
+				}
+			}
+			else { //if new mode
+				if (empty($postedCategories)){
+					throw new Mage_Catalog_Exception('Fill in categories'); //translate if needed
+				}
+			}
+			//<=== end my changes
+	
+			$dateFields = array();
+			$attributes = $product->getAttributes();
+			foreach ($attributes as $attrKey => $attribute) {
+				if ($attribute->getBackend()->getType() == 'datetime') {
+					if (array_key_exists($attrKey, $productData) && $productData[$attrKey] != ''){
+						$dateFields[] = $attrKey;
+					}
+				}
+			}
+			$productData = $this->_filterDates($productData, $dateFields);
+	
+			$product->addData($productData);
+			$product->validate();
+		}
+		catch (Mage_Eav_Model_Entity_Attribute_Exception $e) {
+			$response->setError(true);
+			$response->setAttribute($e->getAttributeCode());
+			$response->setMessage($e->getMessage());
+		} catch (Mage_Core_Exception $e) {
+			$response->setError(true);
+			$response->setMessage($e->getMessage());
+		} catch (Exception $e) {
+			$this->_getSession()->addError($e->getMessage());
+			$this->_initLayoutMessages('adminhtml/session');
+			$response->setError(true);
+			$response->setMessage($this->getLayout()->getMessagesBlock()->getGroupedHtml());
+		}
+	
+		$this->getResponse()->setBody($response->toJson());
+	} 
 }
